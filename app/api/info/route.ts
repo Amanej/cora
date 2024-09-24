26031286d22d10debd132f84eca0a8b33781db03
Aman <aman.mender@gmail.com>
2024-09-24 11:51:36 +0200

    Add post

--- app/api/info/route.ts
+++ app/api/info/route.ts
@@ -14,3 +14,19 @@ export async function GET(req: NextRequest) {
         return NextResponse.json({ message: 'Could not get random name', error }, { status: 500 });
     }
 }
+
+
+export async function POST(req: NextRequest) {
+    try {
+        // console.log("req ", req.headers.keys());
+        console.log("req ", req.url);
+        const getRandomName = () => {
+            const names = ['John', 'Jane', 'Doe', 'Smith'];
+            return names[Math.floor(Math.random() * names.length)];
+        };
+        const randomName = getRandomName();
+        return NextResponse.json(randomName, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Could not get random name', error }, { status: 500 });
+    }
+}
