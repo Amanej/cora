dbb8799864a6ec1296107575d134e4048e7059f3
Aman <aman.mender@gmail.com>
2024-10-20 01:14:37 +0200

    Send slack messages for my own messages

--- services/slack.ts
+++ services/slack.ts
@@ -4,15 +4,17 @@ import { WebClient } from '@slack/web-api';
 const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
 
 export const triggerSlackMessage = async ({name, email, phoneNumber, useCase, lang}:{name?: string, email?: string, phoneNumber: string, useCase: string, lang: string}) => {
-  console.log("triggerSlackMessage called")
-  const text = `*CoraFone Lead* \n Phone: ${phoneNumber} \n Use case: ${useCase} \n Lang: ${lang} \n Name: ${name} \n Email: ${email}.`;
-  await sendSlackMessage(text);
+    if(phoneNumber !== "+4746164687") {
+      const text = `*CoraFone Lead* \n Phone: ${phoneNumber} \n Use case: ${useCase} \n Lang: ${lang} \n Name: ${name} \n Email: ${email}.`;
+      await sendSlackMessage(text);
+    }
 }
 
 export const failedCallSlackMessage = async ({name, email, phoneNumber, useCase, lang}:{name?: string, email?: string, phoneNumber: string, useCase: string, lang: string}) => {
-  console.log("failedCallSlackMessage called")
-  const text = `*CoraFone Call Failed* \n Phone: ${phoneNumber} \n Use case: ${useCase} \n Lang: ${lang} \n Name: ${name} \n Email: ${email}.`;
-  await sendSlackMessage(text);
+  if(phoneNumber !== "+4746164687") {
+    const text = `*CoraFone Call Failed* \n Phone: ${phoneNumber} \n Use case: ${useCase} \n Lang: ${lang} \n Name: ${name} \n Email: ${email}.`;
+    await sendSlackMessage(text);
+  }
 }
 
 
