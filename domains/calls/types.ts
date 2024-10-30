a1d0ca71f1e52e78a576aecfb02550eb665b90f4
Aman <aman.mender@gmail.com>
2024-10-30 17:00:56 +0100

    Add logs for a call

--- domains/calls/types.ts
+++ domains/calls/types.ts
@@ -2,8 +2,16 @@ export type Call = {
     agentId: string;
     phoneNumber: string;
     recordingUrl?: string;
+    // externalId: "e5a1100e-bb9f-4841-9c7f-8a961a5fa6d0",
     status: string;
-    note: string;
+    note?: string; // Should be there
+    outcome: {
+        endingReason: null,
+        typeOfOutcome: null,
+        booleanValue: null,
+        numericValue: null
+    }
     createdAt: Date;
+    transcript: string
     updatedAt: Date;
 }
