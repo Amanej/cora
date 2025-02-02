d8b5ddf02162c680e30926fc9529b16f83e5af78
Aman <aman.mender@gmail.com>
2025-02-02 19:50:51 +0100

    Fix loader

--- app/dashboard/logg/page.tsx
+++ app/dashboard/logg/page.tsx
@@ -1,8 +1,11 @@
 import CallLogs from "@/domains/calls/Logg";
+import { Suspense } from "react";
 
 const CallLogsPage = () => {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
       <CallLogs />
+    </Suspense>
   )
 }
 
