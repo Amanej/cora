093277565817a054673340842e3560224a8242a8
Aman <aman.mender@gmail.com>
2025-02-24 16:06:07 +0100

    Stop calling

--- app/api/call/route.ts
+++ app/api/call/route.ts
@@ -5,12 +5,14 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(request: NextRequest) {
   const { name, email, phoneNumber, useCase, lang }: {name?: string, email?: string, phoneNumber: string, useCase: useCase, lang: LANG} = await request.json();
   try {
+    /*
     const response = await triggerCustomerSurveyCall(phoneNumber, useCase, lang);
     if(response) {
       await triggerSlackMessage({name, email, phoneNumber, useCase, lang});
     } else {
       await failedCallSlackMessage({name, email, phoneNumber, useCase, lang});
     }
+    */
   } catch(err) {
     await failedCallSlackMessage({name, email, phoneNumber, useCase, lang});
   }
