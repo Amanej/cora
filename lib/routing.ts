4ecfa4e44f64f7aea662783aede5d8c993bbceb2
Aman <aman.mender@gmail.com>
2025-03-27 19:42:48 +0100

    Fix routing

--- lib/routing.ts
+++ lib/routing.ts
@@ -1,7 +1,7 @@
 export const ROUTES = {
     HOME: '/',
     DASHBOARD: '/dashboard',
-    ANALYTICS: '/dashboard/analytics/new',
+    ANALYTICS: '/dashboard/analytics',
     MANAGE_AGENTS: '/dashboard/manage',
     CREATE_AGENT: '/dashboard/create-agent',
     LOGG: '/dashboard/logg',
